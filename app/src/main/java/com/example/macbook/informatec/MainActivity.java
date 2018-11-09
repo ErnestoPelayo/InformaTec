1ce8d952d557774a137173cc6517fe2c15b1adcc
JorgeWong6 <konstrictor65@gmail.com>
2018-11-08 21:24:31 -0800

    Buttons
    page change button

--- app/src/main/java/com/example/macbook/informatec/MainActivity.java
+++ app/src/main/java/com/example/macbook/informatec/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.macbook.informatec;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.macbook.informatec.Control.MyAdapter;
@@ -23,10 +26,30 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference reference;
     ArrayList<Events> list;
     MyAdapter adapter;
+    Button btn;
+    Button btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn2 = (Button) findViewById(R.id.btnEventos);
+        btn = (Button) findViewById(R.id.btnConcursos);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivityConcursos.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivityEventos.class));
+            }
+        });
 
         recyclerView =findViewById(R.id.myRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
---
+++ app/src/main/java/com/example/macbook/informatec/MainActivityConcursos.java
@@ -0,0 +1,13 @@
+package com.example.macbook.informatec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityConcursos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_concursos);
+    }
+}
