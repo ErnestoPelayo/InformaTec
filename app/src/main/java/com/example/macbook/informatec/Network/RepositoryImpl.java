6b2c82e0b3d64300656f8a7c565d1fce0a8bb8ae
JorgeWong6 <konstrictor65@gmail.com>
2018-11-23 09:45:51 -0800

    Repository
    Add class RepositoryImpl

---
+++ app/src/main/java/com/example/macbook/informatec/Network/RepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.example.macbook.informatec.Network;
+
+public class RepositoryImpl {
+
+    private FireBaseHelper helper;
+
+        public RepositoryImpl(){
+
+            helper = FireBaseHelper.getInstance();
+
+        }
+
+    }
+
+}
